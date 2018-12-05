e6e2f07de5849fea8eda4838972fedb60b5cf6e9
韩常铭 <han.changming@qq.com>
2018-12-05 17:36:06 +0800

    添加Eureka服务器工程，并整理项目依赖

---
+++ sugar-eureka-server/src/main/java/com/rengu/sugar/sugareurekaserver/SugarEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.sugar.sugareurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarEurekaServerApplication.class, args);
+    }
+}
