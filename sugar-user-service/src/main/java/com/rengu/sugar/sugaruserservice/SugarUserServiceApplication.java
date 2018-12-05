93298a9ff7e096e32af122485085ce2b9de992c1
韩常铭 <han.changming@qq.com>
2018-12-05 15:29:30 +0800

    添加用户服务服务器工程代码

---
+++ sugar-user-service/src/main/java/com/rengu/sugar/sugaruserservice/SugarUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.sugar.sugaruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarUserServiceApplication.class, args);
+    }
+}
