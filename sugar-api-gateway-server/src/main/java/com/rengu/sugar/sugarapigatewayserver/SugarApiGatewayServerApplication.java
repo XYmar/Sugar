970d20f036cc98b2811f66191a7180c3e94671fc
韩常铭 <han.changming@qq.com>
2018-12-07 18:36:32 +0800

    添加API网关服务器的工程

---
+++ sugar-api-gateway-server/src/main/java/com/rengu/sugar/sugarapigatewayserver/SugarApiGatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.sugar.sugarapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarApiGatewayServerApplication.class, args);
+    }
+}
