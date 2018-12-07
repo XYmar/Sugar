a6aec2aca30d90d9bc4bd449d4378a727156f77a
韩常铭 <han.changming@qq.com>
2018-12-07 11:50:56 +0800

    添加授权服务器工程代码

---
+++ sugar-authorization-server/src/main/java/com/rengu/sugar/sugarauthorizationserver/SugarAuthorizationServerApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.sugar.sugarauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarAuthorizationServerApplication.class, args);
+    }
+}
