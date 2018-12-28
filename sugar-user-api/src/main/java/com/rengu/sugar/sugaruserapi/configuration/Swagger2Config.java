b466abc13ad17dacb44355c46978bdf05a2369f1
韩常铭 <han.changming@qq.com>
2018-12-28 17:19:06 +0800

    在User API中添加接口文档

---
+++ sugar-user-api/src/main/java/com/rengu/sugar/sugaruserapi/configuration/Swagger2Config.java
@@ -0,0 +1,37 @@
+package com.rengu.sugar.sugaruserapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sugar User APIs")
+                .description("Sugar User APIs")
+                .contact(new Contact("ChangMing Han", "https://github.com/MagnyCopper", "han.changming@qq.com"))
+                .version("0.0.1")
+                .build();
+    }
+}
